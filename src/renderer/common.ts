94912ae3c608c1b32c16a2cd6aabc48afb663187
takagi <iam.takagi.dev@gmail.com>
2022-02-23 21:02:53 +0900

    [consts.ts] Add: arm andpoint

--- src/renderer/common.ts
+++ src/renderer/common.ts
@@ -1,10 +1,10 @@
 import { Work } from '../types';
-import { API_ENDPOINT } from './consts';
+import { API_ENDPOINT, ARM_ENDPOINT } from './consts';
 
 const randomPick = <T> (array: Array<T>) => array[Math.floor(Math.random() * array.length)]
 
 const pickRandomWork = async () => { 
-    const annictIds = (await fetch(`https://arm.kawaiioverflow.com/api/arm?service=annict`).then((res) => res.json()) as (number | null)[])
+    const annictIds = (await fetch(ARM_ENDPOINT).then((res) => res.json()) as (number | null)[])
     const annictId = randomPick(annictIds.filter(v => v !== null))
     const work = (await fetch(`${API_ENDPOINT}/works/${annictId}`).then(
         (res) => res.json()
--- src/renderer/consts.ts
+++ src/renderer/consts.ts
@@ -1,5 +1,7 @@
-const API_ENDPOINT = "https://anime-title-gacha.iamtakagi.net/api"
+const API_ENDPOINT = "https://anime-title-gacha.iamtakagi.net/api" as const 
+const ARM_ENDPOINT = "https://arm.kawaiioverflow.com/api/arm?service=annict" as const
 
 export {
     API_ENDPOINT,
+    ARM_ENDPOINT
 }
