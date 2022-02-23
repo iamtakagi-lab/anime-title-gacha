d4609e39bb56a6d66992bc41bd25a53da3208649
takagi <iam.takagi.dev@gmail.com>
2022-02-23 09:24:59 +0900

    [consts] Add: API_ENDPOINT

--- src/renderer/consts.ts
+++ src/renderer/consts.ts
@@ -1,5 +1,8 @@
+const isProduction = process.env.NODE_ENV === "production"
 const GACHA_COOLDOWN = 100
+const API_ENDPOINT = isProduction ? "https://anime-title-gacha.iamtakagi.net/api" : "http://localhost:3030/api"
 
 export {
+    API_ENDPOINT,
     GACHA_COOLDOWN
 }
